fe56827377fb3509eeec3f554c42330dd77b3418
Sergio <ccleanermozilla@gmail.com>
2026-06-20 20:41:12 -0500

    Primer commit del proyecto

---
+++ admin/index.php
@@ -0,0 +1,69 @@
+<?php
+include ("../app/config/config.php");
+include ("../app/config/conexion.php");
+include ("../layout/admin/login.php");
+include ("../layout/admin/datos_usuario.php");
+include("../layout/admin/parte1.php");?>
+
+      <main class="app-main">
+        <!--begin::App Content Header-->
+        <div class="app-content-header">
+          <!--begin::Container-->
+          <div class="container-fluid">
+            <!--begin::Row-->
+            <div class="row">
+              <div class="col-sm-6">
+                <h3 class="mb-0">Dashboard</h3>
+              </div>
+              <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-end">
+                  <li class="breadcrumb-item"><a href="#">Home</a></li>
+                  <li class="breadcrumb-item active" aria-current="page">Dashboard</li>
+                </ol>
+              </div>
+            </div>
+            <!--end::Row-->
+          </div>
+          <!--end::Container-->
+        </div>
+        <!--end::App Content Header-->
+          <div class="row">
+            <div class="col-md-4"></div>
+            <div class="col-md-4">
+                <table class="table table table-striped table-hover">
+                  <thead>
+                    <tr>
+                      <th scope="col">Nombres</th>
+                      <th scope="col"><?php echo $nombre; ?></th>
+                    </tr>
+                  </thead>
+                  <tbody class="table-group-divider">
+                    <tr>
+                      <th scope="row">Apellidos</th>
+                      <td><?php echo $apellidos; ?></td>
+                    </tr>
+                    <tr>
+                      <th scope="row">Nombre de usuario</th>
+                      <td><?php echo $nombreusuario; ?></td>
+                    </tr>
+                    <tr>
+                      <th scope="row">Cedula</th>
+                      <td><?php echo $cedula; ?></td>
+                    </tr>
+                    <tr>
+                      <th scope="row">Cargo</th>
+                      <td><?php echo $cargo; ?></td>
+                    </tr>
+                    <tr>
+                      <th scope="row">Fecha de nacimiento</th>
+                      <td><?php echo $fecha; ?></td>
+                    </tr>
+                  </tbody>
+                </table>
+            </div>
+            <div class="col-md-4"></div>
+          </div>
+        <!--end::App Content-->
+      </main>
+
+<?php include("../layout/admin/parte2.php"); ?>
---
+++ app/config/config.php
@@ -0,0 +1,18 @@
+<?php
+
+//localmente
+define('BD_SERVIDOR','localhost');
+define('BD_USUARIO','root');
+define('BD_PASSWORD','');
+define('BD_SISTEMA','bd_sis_biblioteca');
+
+
+//servidor
+/*
+define("BD_SERVIDOR","localhost");
+define("BD_USUARIO","root");
+define("BD_PASSWORD"," ");
+define("BD_SISTEMA","bd_sis_biblioteca");
+*/
+
+$URL='http://localhost/www.sis_biblioteca.com';
