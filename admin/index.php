f130e7e3b087f6cc65448e55045fa20aeae72608
Sergio <ccleanermozilla@gmail.com>
2026-06-28 18:31:10 -0500

    Cambio de titulo

--- admin/index.php
+++ admin/index.php
@@ -13,7 +13,7 @@ include("../layout/admin/parte1.php");?>
             <!--begin::Row-->
             <div class="row">
               <div class="col-sm-6">
-                <h3 class="mb-0">Dashboard</h3>
+                <h3 class="mb-0">INFORMACION DEL USUARIO</h3>
               </div>
               <div class="col-sm-6">
                 <ol class="breadcrumb float-sm-end">
