7d7b539c7598124943d25dacf74ca42dcefb4108
Sergio <ccleanermozilla@gmail.com>
2026-07-05 20:32:52 -0500

    Agregar módulo de libros: categorías, subcategorías, tipos, temas, autor y PDF

--- admin/libros/create_tema.php
+++ admin/libros/create_tema.php
@@ -1,6 +1,6 @@
 <?php
-require_once (__DIR__ . "/../../app/config/config.php");
-require_once (__DIR__ . "/../../app/config/conexion.php");
+include ("../../app/config/config.php");
+include ("../../app/config/conexion.php");
 header('Content-Type: application/json');
 
 $data = json_decode(file_get_contents('php://input'), true);
